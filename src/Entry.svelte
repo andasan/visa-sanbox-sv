22df4c8ea66a3cae516015750a0d245f9fb65e08
Francois Polo <andasan@gmail.com>
2022-03-02 13:57:20 -0800

    :building_construction: create modal component

--- src/Entry.svelte
+++ src/Entry.svelte
@@ -1,47 +1,41 @@
 <script>
-  let people = [
-    { id: 0, name: "hoge", color: "green", type: "mage", hp: 50 },
-    { id: 1, name: "boke", color: "orange", type: "crusader", hp: 50 },
-    { id: 2, name: "moge", color: "pink", type: "priest", hp: 50 },
-  ];
+    import Modal from './Components/Modal.svelte'
 
-  const handleClick = (id) => {
-    people = people.filter((person) => person.id !== id);
-  };
-
-  let num = 5;
-</script>
-
-<!-- Conditional -->
-{#if num > 20}
-  <p>Greater than 10</p>
-{:else}
-  <p>Lesser than 20</p>
-{/if}
-
-<main>
-  <!-- Loops -->
-  {#each people as person (person.id)}
-    <div>
-      <h4 style="color:{person.color}">{person.name}</h4>
-      <p>{person.type}</p>
-      <button on:click={() => handleClick(person.id)}>Delete</button>
-    </div>
-  {:else}
-    <p>There's nothing here...</p>
-  {/each}
-</main>
-
-<style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-  @media (min-width: 640px) {
+    let people = [
+      { id: 0, name: "hoge", color: "green", type: "mage", hp: 50 },
+      { id: 1, name: "boke", color: "orange", type: "crusader", hp: 50 },
+      { id: 2, name: "moge", color: "pink", type: "priest", hp: 50 },
+    ];
+  
+    const handleClick = (id) => {
+      people = people.filter((person) => person.id !== id);
+    };
+  </script>
+  
+  <Modal />
+  <main>
+    {#each people as person (person.id)}
+      <div>
+        <h4 style="color:{person.color}">{person.name}</h4>
+        <p>{person.type}</p>
+        <button on:click={() => handleClick(person.id)}>Delete</button>
+      </div>
+    {:else}
+      <p>There's nothing here...</p>
+    {/each}
+  </main>
+  
+  <style>
     main {
-      max-width: none;
+      text-align: center;
+      padding: 1em;
+      max-width: 240px;
+      margin: 0 auto;
     }
-  }
-</style>
+    @media (min-width: 640px) {
+      main {
+        max-width: none;
+      }
+    }
+  </style>
+  
