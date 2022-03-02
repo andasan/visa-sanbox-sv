5ac5747d1342bf3928b901e652bf4f9e51fee337
Francois Polo <andasan@gmail.com>
2022-03-02 10:44:40 -0800

    :boom: two-way binding

--- src/Entry.svelte
+++ src/Entry.svelte
@@ -1,38 +1,39 @@
 <script>
-	export let name
-	let state = 'init'
+  export let name;
+  let state = "init";
 
-	const handleClick = () => state = 'updated'
-	const handleInputChange = (e) => {
-		state = e.target.value 
-	}
+  const handleClick = () => (state = "updated");
+  const handleInputChange = (e) => {
+    state = e.target.value;
+  };
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>{state}</p>
-	<button on:click={handleClick}>Update state</button>
-	<input type="text" on:input={handleInputChange} value={state}>
+  <h1>Hello {name}!</h1>
+  <p>{state}</p>
+  <button on:click={handleClick}>Update state</button>
+  <!-- <input type="text" on:input={handleInputChange} value={state} /> -->
+  <input type="text" bind:value={state}>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
