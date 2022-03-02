f2144282e19d6a4eff2463fed6c666af68a292a3
Francois Polo <andasan@gmail.com>
2022-03-02 10:32:44 -0800

    :beers: user input and data binding early stages

--- src/Entry.svelte
+++ src/Entry.svelte
@@ -3,12 +3,14 @@
 	let state = 'init'
 
 	const handleClick = () => state = 'updated'
+	const handleInputChange = (e) => state = e.target.value 
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>{state}</p>
 	<button on:click={handleClick}>Update state</button>
+	<input type="text" on:input={handleInputChange}>
 </main>
 
 <style>
