1458d6579e9481519d5c0096c325d65e47486a83
Francois Polo <andasan@gmail.com>
2022-03-02 14:34:27 -0800

    :lipstick: css and conditional styles

--- src/Components/Modal.svelte
+++ src/Components/Modal.svelte
@@ -1,13 +1,32 @@
 <script>
     let showModal = true
+    let isPromo = true
 </script>
 
 <style>
-    /* your styles go here */
+    .backdrop{
+        width: 100%;
+        height: 100%;
+        position: fixed;
+        background-color: rgba(0,0,0,0.8);
+    }
+    .modal{
+        padding: 10px;
+        border-radius: 10px;
+        max-width: 400px;
+        margin: 10% auto;
+        text-align: center;
+        background-color: white;
+    }
+
+    .promo .modal{
+        background-color: crimson;
+        color: white;
+    }
 </style>
 
 {#if showModal}
-<div class="backdrop">
+<div class="backdrop" class:promo={isPromo}>
     <div class="modal">
         <p>Sign up for offers</p>
     </div>
