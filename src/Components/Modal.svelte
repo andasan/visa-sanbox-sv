a733dd26c9cd64d695af5128a9c1bcbdbb13084e
Francois Polo <andasan@gmail.com>
2022-03-02 15:13:55 -0800

    :sparkles: event modifier usage on closing of modal when clicking outside of modal area

--- src/Components/Modal.svelte
+++ src/Components/Modal.svelte
@@ -1,35 +1,46 @@
 <script>
-    export let showModal = false
-    export let isPromo = false
-    export let message = " Empty modal message"
+  /**
+   * Event Modifiers
+   *
+   * @ once - makes sure the event can only fire once (removes handler)
+   * @ preventDefault - prevent the default action (run e.preventDefaul())
+   * @ self - only fires the event if the clicked element is the target
+   */
+
+  export let showModal = false;
+  export let isPromo = false;
+  export let message = " Empty modal message";
 </script>
 
-<style>
-    .backdrop{
-        width: 100%;
-        height: 100%;
-        position: fixed;
-        background-color: rgba(0,0,0,0.8);
-    }
-    .modal{
-        padding: 10px;
-        border-radius: 10px;
-        max-width: 400px;
-        margin: 10% auto;
-        text-align: center;
-        background-color: white;
-    }
-
-    .promo .modal{
-        background-color: crimson;
-        color: white;
-    }
-</style>
-
 {#if showModal}
-<div class="backdrop" class:promo={isPromo} on:click>
+  <div class="backdrop" class:promo={isPromo} on:click|self>
     <div class="modal">
-        <p>{message}</p>
+      <p>{message}</p>
+
+      <!-- <button on:click>Cancel</button>
+      <button on:click>Proceed</button> -->
     </div>
-</div>
+  </div>
 {/if}
+
+<style>
+  .backdrop {
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    background-color: rgba(0, 0, 0, 0.8);
+  }
+  .modal {
+    padding: 10px;
+    border-radius: 10px;
+    max-width: 400px;
+    margin: 10% auto;
+    text-align: center;
+    background-color: white;
+  }
+
+  .promo .modal {
+    background-color: crimson;
+    color: white;
+  }
+</style>
