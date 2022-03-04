6a3ad06a07fc6e5c11663af680a8f61f1ed38ac5
Francois Polo <andasan@gmail.com>
2022-03-04 15:11:04 -0800

    :sparkles: reusable tabs component

--- src/Entry.svelte
+++ src/Entry.svelte
@@ -1,78 +1,26 @@
 <script>
-  import Modal from "./Components/Modal.svelte";
-  import AddPlayerForm from "./Components/AddPlayerForm.svelte";
+  import Header from "./components/layout/Header.svelte";
+  import Footer from "./components/layout/Footer.svelte"
+  import Tabs from "./components/widgets/tabs/Tabs.svelte"
 
-  let showModal = false;
-  let showAddModal = false;
-  let selectedPlayer = {};
+  let tabItems = ['Current Polls', 'Add New Poll']
+  let activeItem = 'Current Polls'
 
-  let players = [
-    { id: 0, name: "hoge", color: "green", classes: "mage", hp: 50 },
-    { id: 1, name: "boke", color: "orange", classes: "crusader", hp: 50 },
-    { id: 2, name: "moge", color: "pink", classes: "priest", hp: 50 },
-  ];
-
-  const addPlayer = (e) => {
-    players = [...players, e.detail];
-    showAddModal = !showAddModal;
-  };
-
-  const handleAddPlayer = () => {
-    showAddModal = !showAddModal;
-  };
-
-  const toggleAddPlayerModal = () => {
-    showAddModal = !showAddModal;
-  };
-
-  const handleDelete = (id) => {
-    players = players.filter((p) => p.id !== id);
-    showModal = !showModal;
-  };
-
-  const toggleModal = (u) => {
-    selectedPlayer = u;
-    showModal = !showModal;
-  };
+  const handleTabChange = (e) => {
+    activeItem = e.detail
+  }
 </script>
 
-<Modal isPromo={true} {selectedPlayer} {showModal} on:click={toggleModal}>
-  <h3>
-    ⚠️ Are you sure you want to delete <b style="color:yellow"
-      >{selectedPlayer.name}</b
-    >?!
-  </h3>
-  <button on:click={toggleModal}>Cancel</button>
-  <button on:click={() => handleDelete(selectedPlayer.id)}>Proceed</button>
-</Modal>
-
-<Modal showModal={showAddModal} on:click={toggleAddPlayerModal}>
-  <AddPlayerForm on:addPlayer={addPlayer} />
-</Modal>
-
+<Header />
 <main>
-  <button on:click={handleAddPlayer}>Add A New Player</button>
-  {#each players as player (player.id)}
-    <div>
-      <h4 style="color:{player.color}">{player.name}</h4>
-      <p>{player.classes}</p>
-      <button on:click={() => toggleModal(player)}>Delete</button>
-    </div>
-  {:else}
-    <p>There's nothing here...</p>
-  {/each}
+  <Tabs {tabItems} {activeItem} on:tabChange={handleTabChange} />
 </main>
+<Footer />
 
 <style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
+  main{
+    /* max-width: 960px; */
+    max-width: 400px;
+    margin: 40px auto;
   }
 </style>
---
+++ src/components/widgets/tabs/Tabs.svelte
@@ -0,0 +1,44 @@
+<script>
+    import { createEventDispatcher } from 'svelte'
+
+    export let tabItems
+    export let activeItem
+
+    const dispatch = createEventDispatcher()
+</script>
+
+<div class="tabs">
+<ul>
+    {#each tabItems as tabItem}
+        <li on:click={() => dispatch('tabChange', tabItem)}>
+            <div class:active={tabItem === activeItem}>{tabItem}</div>
+        </li>
+    {/each}
+</ul>
+</div>
+
+<style>
+    .tabs {
+        margin-bottom: 40px;
+    }
+
+    ul{
+        display: flex;
+        justify-content: center;
+        padding: 0;
+        list-style: none;
+    }
+
+    li{
+        margin: 0 16px;
+        font-size: 18px;
+        color: #555;
+        cursor: pointer;
+    }
+    
+    .active {
+        color: #d91b42;
+        border-bottom: 2px solid #d91b42;
+        padding-bottom: 8px;
+    }
+</style>
